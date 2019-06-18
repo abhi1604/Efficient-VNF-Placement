d4678b67b5593f69f5c29c7bd2db4082203068ef
abhi1604 <es15btech11002@iith.ac.in>
2019-06-18 12:16:13 +0530

    Changed longest path logic

--- dfs.h
+++ dfs.h
@@ -1,68 +1,50 @@
 #include<bits/stdc++.h> 
 using namespace std; 
   
+
+float max_delay = FLT_MIN;
+vector<pair<int, int>> max_path;
+
 // visited[] array to make nodes visited 
 // src is starting node for DFS traversal 
 // prev_delay is sum of delays till current node 
-// total_delay is pointer which stores the maximum delay 
+// max_delay is pointer which stores the maximum delay 
 // value after DFS traversal 
-vector<pair<int, int>> DFS(vector<vector<struct LinkInfo>> graph, int node, float prev_delay, float *total_delay, vector <bool> &visited, vector<pair<int,int>> path, struct Request request)
+void DFS(vector<vector<struct LinkInfo>> graph, int node, vector <bool> &visited, vector<pair<int,int>> &cur_path, struct Request request, float cur_path_delay)
 {
-    vector<pair<int, int>> temp_path(path);
 
     pair<int, int> temp1;
 
     temp1.first = node;
 
-    if(request.source==node||request.destination==node)
-        temp1.second = -2; // to indicate, src, dest
-    else
-        temp1.second = -1;
-
-    temp_path.push_back(temp1);
-
-    // Mark the node node visited 
-    visited[node] = 1; 
-
     if(request.destination==node)
-        return temp_path;
-
-    // curr_delay is for delay from node to its adjacent nodes 
-    float curr_delay = 0; 
-
-    // Adjacent is struct type which stores link info between src and adjacent nodes  
-    struct LinkInfo adjacent; 
-
-    // Traverse all adjacent nodes
-    for (int i=0; i<graph[node].size(); i++) 
     {
-        // Adjacent node 
-        adjacent = graph[node][i]; 
-
-        // If node is not visited 
-        vector<pair<int, int>> return_path;
-        if (!visited[adjacent.node2]) 
-        { 
-            // Total delay from current node to its adjacent 
-            curr_delay = prev_delay + adjacent.delay; 
-
-            // Call DFS for adjacent node
-            return_path = DFS(graph, adjacent.node2, curr_delay, total_delay,  visited, temp_path, request); 
-        } 
-    
-        // If total delay till now greater than previous delay then update it 
-        if ((*total_delay) < curr_delay && return_path.size()!=0 && curr_delay<request.delay)
-        { 
-            if(return_path[return_path.size()-1].first==request.destination)
-            {
-                temp_path = return_path;
-                *total_delay = curr_delay; 
-            }
+        if(max_delay < cur_path_delay)   
+        {
+            max_delay = cur_path_delay;
+            max_path = cur_path;
         }
-        // make curr_delay = 0 for next adjacent node
-        curr_delay = 0; 
+        return;
     }
-    return temp_path;
+
+    float new_path_delay;
+
+    for(auto x: graph[node])
+    {
+        if(!visited[x.node2])
+        {
+            visited[x.node2] = 1;
+            cur_path.push_back(make_pair(x.node2, -1));
+            new_path_delay = cur_path_delay + x.delay;
+
+            DFS(graph, x.node2, visited, cur_path, request, new_path_delay);
+
+            visited[x.node2] = 0;
+
+            cur_path.pop_back();
+        }
+    }
+    return ;
 } 
 
 struct path_info longestPath(struct Request request, vector<vector<struct LinkInfo>> graph, map<int, vector<int>> vnfNodes, vector<struct Node> local_nodes, string algo_name)
@@ -74,18 +56,22 @@ struct path_info longestPath(struct Request request, vector<vector<struct LinkIn
     int throughput = request.throughput;
     float delay = request.delay;
 
-    float total_delay = FLT_MIN; 
-
     // initialize visited array with 0 
     vector< bool > visited(V, false); 
 
     vector<pair<int, int>> complete_path;
     vector<pair<int, int>> temp_path;
 
-    // Call DFS for src vertex i 
-    complete_path = DFS(graph, src, 0, &total_delay, visited, temp_path, request); 
 
-    if(total_delay>delay || total_delay == FLT_MIN)
+    temp_path.push_back(make_pair(src, -2));
+    visited[src]=1;
+
+    // Call DFS for src vertex i 
+    DFS(graph, src, visited, temp_path, request, 0); 
+
+    complete_path = temp_path;
+
+    if(max_delay>delay || max_delay == FLT_MIN)
     {
         struct path_info selected_path_info;
         vector<pair<int, int>> temp;
@@ -102,7 +88,7 @@ struct path_info longestPath(struct Request request, vector<vector<struct LinkIn
     }
 
     struct path_info selected_path_info;
-    selected_path_info.delay = total_delay;
+    selected_path_info.delay = max_delay;
     selected_path_info.path_with_type = complete_path;
 
     return selected_path_info;
